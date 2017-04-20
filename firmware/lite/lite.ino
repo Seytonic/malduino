8b47c099e61e760687c189d003d1e6f55445db54
Stefan Kremser <stef.1497@gmail.com>
2017-04-20 18:26:38 +0200

    Added malduino lite firmware

---
+++ firmware/lite/lite.ino
@@ -0,0 +1,31 @@
+#include "Keyboard.h"
+
+#define buttonPin 6
+int defaultDelay = 4;
+int defaultCharDelay = 5;
+
+void typeKey(int key){
+  Keyboard.press(key);
+  delay(defaultCharDelay);
+  Keyboard.release(key);
+}
+
+void setup(){
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  if(digitalRead(buttonPin) == LOW){
+    /* ----- Script-Begin (just a little Hello World example here) ----- */
+    Keyboard.begin();
+    
+    delay(1000);
+  
+    delay(defaultDelay);
+    Keyboard.print("Hello World!");
+  
+    delay(defaultDelay);
+    /* ----- Script-End ----- */
+    Keyboard.end();
+  }
+}
+
+void loop() {}
