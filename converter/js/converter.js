f07307bf9038d446cdd37408f1bf306e959c707c
inetphantom <git@inetphantom.ch>
2020-08-28 14:05:38 +0200

    Fix issues on de_CH also tested

--- converter/js/converter.js
+++ converter/js/converter.js
@@ -228,6 +228,7 @@ class malduinoScriptConverter{
 						langFile = result;
 						if(lang == "es_ES") keyboardCPP = "#define ADD_ALTGR\r\n";
 						if(lang == "de_DE") keyboardCPP = "#define ADD_ALTGR\r\n";
+						if(lang == "de_CH") keyboardCPP = "#define ADD_ALTGR\r\n";
 						if(lang == "it_IT") keyboardCPP = "#define ADD_ALTGR\r\n";
 						if(lang == "sv_SE") keyboardCPP = "#define ADD_ALTGR\r\n";
 						if(lang == "fi_FI") keyboardCPP = "#define ADD_ALTGR\r\n";
